4dd03711115c2bf6f9f9e0fbb5a55c7c14cc5ddc
joaco <joaco@DESKTOP-KCPCDBH>
2024-10-09 14:52:40 -0300

    Agregar archivos de proyecto.

---
+++ Verdeo/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Agregar servicios al contenedor.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configurar el pipeline de solicitudes HTTP.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
